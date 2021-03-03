3fcda14b093aaea5eb5adb501d8d3a1c9d42235a
Erol <64768508+profiteroles@users.noreply.github.com>
2021-03-03 11:55:40 +0800

    1st-App-Smiley-Face
    One of the applications they thought us in our Diploma in Software Development. Nothing to Exciting Simply Creates a smiley face on the console.

---
+++ Java-1st-App-Smiley-Face/src/smileyface/SmileyFace.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smileyface;
+
+/**
+ *
+ * @author ProFitErol
+ */
+public class SmileyFace {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("         XXX   ");
+        System.out.println("        XXXXX");
+        System.out.println("       XXXXXXX");
+        System.out.println("     xxOOOxxOOOxx");
+        System.out.println("    xxxxOxxxxOxxxx");
+        System.out.println("                    ");
+        System.out.println("                    ");
+        System.out.println("  X               X");
+        System.out.println("    XX         XX");
+        System.out.println("      XXXXXXXXX");
+        System.out.println("         XXX");
+        System.out.println("               ");
+        System.out.println("         XXX");
+        System.out.println("          X");
+    }
+    
+}
